b51ae856ae29cfdbb2420362bcf74fdcb87e52c7
Sneharani <sneharani1728@gmail.com>
2026-08-14 08:57:39 +0530

    Replace HTML login with Java main class
    Removed HTML login form and added a Java main class with welcome messages.

---
+++ main/main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to YuvaMart");
+        System.out.println("Registration Module Completed");
+        System.out.println("Login Module Completed");
+    }
+}
